a8e0ecfa810c08d74d220977793406a0bc53c4c0
Paul Tangen <ptangen@ptangen.com>
2016-10-17 14:57:41 -0400

    Complete the scroll view lab.

--- ScrollViewFun/ViewController.swift
+++ ScrollViewFun/ViewController.swift
@@ -9,9 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView1: UIScrollView!
+    @IBOutlet weak var stackView1: UIStackView!
+    @IBOutlet weak var image1: UIStackView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.scrollView1.topAnchor.constraint(equalTo:      self.view.topAnchor).isActive = true
+        self.scrollView1.bottomAnchor.constraint(equalTo:   self.view.bottomAnchor).isActive = true
+        self.scrollView1.leftAnchor.constraint(equalTo:     self.view.leftAnchor).isActive = true
+        self.scrollView1.rightAnchor.constraint(equalTo:    self.view.rightAnchor).isActive = true
+        self.scrollView1.translatesAutoresizingMaskIntoConstraints = false // required for code that controls layout
+        
+        self.stackView1.topAnchor.constraint(equalTo:       self.scrollView1.topAnchor).isActive = true
+        self.stackView1.bottomAnchor.constraint(equalTo:    self.scrollView1.bottomAnchor).isActive = true
+        self.stackView1.leftAnchor.constraint(equalTo:      self.scrollView1.leftAnchor).isActive = true
+        self.stackView1.rightAnchor.constraint(equalTo:     self.scrollView1.rightAnchor).isActive = true
+        self.stackView1.widthAnchor.constraint(equalTo:     self.scrollView1.widthAnchor, multiplier: 5).isActive = true
+        self.stackView1.heightAnchor.constraint(equalTo:    self.scrollView1.heightAnchor).isActive = true
+        self.stackView1.translatesAutoresizingMaskIntoConstraints = false // required for code that controls layout
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
         
         
